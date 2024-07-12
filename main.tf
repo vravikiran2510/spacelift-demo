5c12199de115ebd7d9dfe022c68b49e4f28d0512
Ravikiran V <ravikiran.v@ltts.com>
2024-07-12 11:09:50 +0530

    third commit

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
-｝
+}
 resource "aws_instance" "app_server" {
     ami             = "ami-02396cdd13e9a1257"
     instance_type   = "t2.micro"
 
     tags = {
         Name        = "app-server"
-    ｝
-｝
+    }
+}
--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,6 @@ output "instance_id" {
 output "instance_public_ip" {
     description = "Public IP address of the EC2 instance"
     value       = aws_instance.app_server.public_ip
-｝
+}
 
 # Additional Comments
